77a8fd0a72053a17c408b8fb8e2b9cc86b5198ab
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2018-11-27 11:50:24 -0500

    Update

--- lessons/14_week_modelsandTextAnalysis/models/lungsCap_i.r
+++ lessons/14_week_modelsandTextAnalysis/models/lungsCap_i.r
@@ -1,4 +1,4 @@
-# data: 27 Nov 2018
+# date: 27 Nov 2018
 
 library(tidyverse)
 library(psych)
@@ -8,4 +8,20 @@ dataLungCap <- read.csv(lc)
 View(dataLungCap)
 attach(dataLungCap)
 mod <- lm(LungCap ~ Age + Height)
+
+
+# model creation
+mod <- lm(LungCap ~ Age + Height)
+mod_age <- lm(LungCap ~ Age)
+mod_height <- lm(LungCap ~ Height)
+
+
+# get a report of the models
 summary(mod)
+summary(mod_age)
+summary(mod_Height)
+
+
+mod2 <- lm(LungCap ~ Age + Height + Smoke + Gender + Caesarean)
+summary(mod2)
+plot(mod2) # check the four visuals!
